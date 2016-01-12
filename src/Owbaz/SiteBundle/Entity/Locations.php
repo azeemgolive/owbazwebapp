8fed8c22e9eaacf831249ed65fd03673fbae15e1
RahulDembani <rahool@golive.com.pk>
2016-01-12 11:23:56 +0500

    created location entity in site bundle owbaz

---
+++ src/Owbaz/SiteBundle/Entity/Locations.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Owbaz\SiteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Locations
+ *
+ * @ORM\Table(name="locations")
+ * @ORM\Entity(repositoryClass="Owbaz\SiteBundle\Repository\LocationsRepository")
+ */
+class Locations
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="location_name", type="string", length=255)
+     */
+    private $locationName;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updatedAt", type="datetime")
+     */
+    private $updatedAt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set locationName
+     *
+     * @param string $locationName
+     *
+     * @return Locations
+     */
+    public function setLocationName($locationName)
+    {
+        $this->locationName = $locationName;
+
+        return $this;
+    }
+
+    /**
+     * Get locationName
+     *
+     * @return string
+     */
+    public function getLocationName()
+    {
+        return $this->locationName;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Locations
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Locations
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
+
